e6fdfc58e4310236dd04d4da240d4398017da6a9
daviddoingthingsright <daviddoingthingsright@gmail.com>
2025-11-29 20:17:36 -0800

    add mutmut mutation testing

--- check.sh
+++ check.sh
@@ -3,4 +3,7 @@
 poetry run ruff check .
 poetry run mypy .
 poetry run pytest .
+# Currently, mutmut mutation test has around 30% survival rate.
+# This is not good enough and I may want to improve it in the future
+poetry run mutmut run
 poetry check
